16cd176c79badf1d973d9827ddd94179feca7fcd
nilshae <nilshaeusler@gmail.com>
2016-10-30 19:47:43 +0100

    Added implementaion of Coordinate class and tests

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -1,7 +1,77 @@
+/*
+ * This file is part of the Wahlzeit photo rating application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package org.wahlzeit.model;
 
-/**
- * Created by Nils on 24/10/16.
- */
 public class Coordinate {
+    public static final double EARTH_RADIUS = 6371; //Approximation in km
+    private final double latitude;
+    private final double longitude;
+
+    /**
+     * This is the standard constructor of the Coordinate class.
+     * @param latitude in degree
+     * @param longitude in degree
+     */
+    public Coordinate(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    /**
+     * Returns the latitude.
+     * @return latitude in degree
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Returns the longitude.
+     * @return longitude in degree
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * Computes the distance between this Coordinate and an other Coordinate.
+     * The implementation follows https://www.wikiwand.com/en/Haversine_formula#/The_haversine_formula
+     * @param otherCoordinate the Coordinate to calculate the distance with.
+     * @return distance between the two Coordinates
+     */
+    public double getDistance(Coordinate otherCoordinate) {
+        double deltaLatitude = Math.toRadians(latitude - otherCoordinate.getLatitude());
+        double deltaLongitude = Math.toRadians(longitude - otherCoordinate.getLongitude());
+        double firstLatitudeInRad = Math.toRadians(latitude);
+        double secondLatitudeInRad = Math.toRadians(otherCoordinate.getLatitude());
+
+        double haversineCentralAngle = haversine(deltaLatitude) + Math.cos(firstLatitudeInRad) * Math.cos(secondLatitudeInRad) * haversine(deltaLongitude);
+        return 2 * EARTH_RADIUS * Math.asin(Math.sqrt(haversineCentralAngle));
+    }
+
+    /**
+     * Computes the haversine for a given angle in radians.
+     * The implementation follows https://www.wikiwand.com/en/Haversine_formula#/The_haversine_formula
+     * @param angleInRand the angle in radians
+     * @return the haversine for the given angle.
+     */
+    private double haversine(double angleInRand) {
+        return (1 - Math.cos(angleInRand))/ 2;
+    }
 }
--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -1,8 +1,47 @@
-import static org.junit.Assert.*;
+package org.wahlzeit.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- * Created by Nils on 24/10/16.
+ * Test class for {@link Coordinate}.
  */
 public class CoordinateTest {
 
+    @Test
+    public void testConstructor() {
+        Coordinate coordinate = new Coordinate(100, 200);
+
+        assertEquals(coordinate.getLatitude(), 100, 0);
+        assertEquals(coordinate.getLongitude(), 200, 0);
+    }
+
+    @Test
+    public void testGetDistance() {
+        Coordinate coordinate1 = new Coordinate(50.2064932, -117.1873205);
+        Coordinate coordinate2 = new Coordinate(50.2064932, -117.2373205);
+
+        double distance = coordinate1.getDistance(coordinate2);
+
+        assertEquals(distance, 3.5, 0.5);
+    }
+
+    @Test
+    public void testGetDistanceSameLocation() {
+        Coordinate coordinate1 = new Coordinate(1.0, 1.0);
+        Coordinate coordinate2 = new Coordinate(1.0, 1.0);
+
+        double distance = coordinate1.getDistance(coordinate2);
+
+        assertEquals(distance, 0.0, 0);
+    }
+
+    @Test
+    public void testGetDistanceCommutative() {
+        Coordinate coordinate1 = new Coordinate(1.0, 1.0);
+        Coordinate coordinate2 = new Coordinate(1.0, 1.0);
+
+        assertEquals(coordinate1.getDistance(coordinate2), coordinate2.getDistance(coordinate1), 0);
+    }
 }
