3414bedc5c3605203f9e9fe59857a836e484563c
nilshae <nilshaeusler@gmail.com>
2016-10-30 19:41:53 +0100

    Added Location and Coordinate class with tests

---
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -0,0 +1,7 @@
+package org.wahlzeit.model;
+
+/**
+ * Created by Nils on 24/10/16.
+ */
+public class Coordinate {
+}
---
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nils on 24/10/16.
+ */
+public class CoordinateTest {
+
+}
