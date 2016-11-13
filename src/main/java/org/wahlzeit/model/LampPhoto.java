77f31f08d8e96ef088126db6af84c2b493758684
nilshae <nilshaeusler@gmail.com>
2016-11-13 16:57:09 +0100

    added comments for fileds so specifiy a LampPhoto

--- src/main/java/org/wahlzeit/model/LampPhoto.java
+++ src/main/java/org/wahlzeit/model/LampPhoto.java
@@ -2,6 +2,12 @@ package org.wahlzeit.model;
 
 public class LampPhoto extends Photo {
 
+    // TODO: add fields that make lamps special.
+    // e.g. wastage in watt
+    // bool is
+    // enum Material {wood, metal, plastic}
+    // enum Type {floorlamp, hanging lamp, table lamp, ...}
+
     /**
      * This is the standard constructor of the LampPhoto class.
      * @methodtype constructor
