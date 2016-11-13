2be5becf57588c016838ddc060b19847a9d42287
nilshae <nilshaeusler@gmail.com>
2016-11-13 13:42:48 +0100

    fixed suite where the actual class was added instead of the test class

--- src/test/java/org/wahlzeit/model/persistence/PersistenceTestSuite.java
+++ src/test/java/org/wahlzeit/model/persistence/PersistenceTestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         AbstractAdapterTest.class,
-        DatastoreAdapter.class,
+        DatastoreAdapterTest.class,
         GcsAdapterTest.class
 })
 
