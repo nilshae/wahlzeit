25bb6c288d9c47111ad3c384bab42aeb0bb2042e
nilshae <nilshaeusler@gmail.com>
2016-11-19 23:21:18 +0100

    added implementation of CartesianCoordinate + tests

--- src/main/java/org/wahlzeit/model/CartesianCoordinate.java
+++ src/main/java/org/wahlzeit/model/CartesianCoordinate.java
@@ -20,7 +20,7 @@ package org.wahlzeit.model;
 
 public class CartesianCoordinate implements Coordinate {
 
-    // x, y, z represent the position on earths surface. The origin is the center of earth.
+    // x, y, z represent a position on earths surface. The origin is the center of earth.
     private final double x;
     private final double y;
     private final double z;
@@ -36,20 +36,71 @@ public class CartesianCoordinate implements Coordinate {
         this.x = x;
         this.y = y;
         this.z = z;
+
+        if (!isValidCoordinate()) {
+            throw new IllegalArgumentException("CartesianCoordinate does not lie of earths surface.");
+        }
     }
 
     /**
      * Computes the distance between this Coordinate and an other Coordinate.
+     * This implementation makes use of the implementation form the SphericalCoordinate class.
      * @param otherCoordinate the Coordinate to calculate the distance with.
      * @return distance between the two Coordinates
      */
     @Override
     public double getDistance(Coordinate otherCoordinate) {
-        return 0;
+        SphericalCoordinate thisAsSpherical = this.convertToSpherical();
+        SphericalCoordinate otherAsSpherical = otherCoordinate.convertToSpherical();
+
+        return thisAsSpherical.getDistance(otherAsSpherical);
     }
 
+    /**
+     * Converts the this instance to a SphericalCoordinate.
+     * @return this Coordinate as SphericalCoordinate
+     */
     @Override
     public SphericalCoordinate convertToSpherical() {
-        return null;
+        double radius = Math.sqrt(x*x + y*y + z*z);
+        double latitude = Math.atan2(y, x);
+        double longitude =  Math.atan2(Math.sqrt( Math.pow(x,2) + Math.pow(y,2) ), z);
+
+        return new SphericalCoordinate(Math.toDegrees(latitude), Math.toDegrees(longitude));
+    }
+
+    /**
+     * Returns the prim value x.
+     * @methodtype get
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * Returns the prim value y.
+     * @methodtype get
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * Returns the prim value z.
+     * @methodtype get
+     */
+    public double getZ() {
+        return z;
+    }
+
+
+    /**
+     * Checks if given position is located on earths surface with respect to an error value.
+     * @return boolean
+     */
+    private boolean isValidCoordinate() {
+        double lengthOfVector = Math.sqrt(x*x + y*y + z*z);
+
+        return Math.abs(lengthOfVector - Coordinate.EARTH_RADIUS_KM) <= 1e-7;
     }
 }
--- src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
@@ -7,12 +7,67 @@ import static org.junit.Assert.*;
 public class CartesianCoordinateTest {
 
     @Test
-    public void getDistance() {
+    public void testConstructor() {
+        CartesianCoordinate coordinate = new CartesianCoordinate(-6371, 0, 0);
 
+        assertEquals(-6371, coordinate.getX(), 0);
+        assertEquals(0, coordinate.getY(), 0);
+        assertEquals(0, coordinate.getZ(), 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPositionInsideEarth() {
+        CartesianCoordinate coordinate = new CartesianCoordinate(100, 100, 100);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPositionInOuterSpace() {
+        CartesianCoordinate coordinate = new CartesianCoordinate(10000, 10000, 10000);
     }
 
     @Test
-    public void convertToSpherical() {
+    public void testGetDistance() {
+        // This is only a simple test, because the getDistance() extensively
+        // tested in SphericalCoordinateTest
 
+        CartesianCoordinate coordinate1 = new CartesianCoordinate(6371, 0, 0);
+        CartesianCoordinate coordinate2 = new CartesianCoordinate(0, 6371, 0);
+
+        double distance = coordinate1.getDistance(coordinate2);
+
+        assertEquals(10007.54, distance, 0.01);
+    }
+
+    @Test
+    public void testOnXAxisConvertToSpherical() {
+        CartesianCoordinate cartesianCoordinate = new CartesianCoordinate(6371, 0, 0);
+        SphericalCoordinate sphericalCoordinate = new SphericalCoordinate(0, 90);
+
+        SphericalCoordinate convertedCoordinate = cartesianCoordinate.convertToSpherical();
+
+        assertEquals(sphericalCoordinate.getLongitude(), convertedCoordinate.getLongitude(), 0);
+        assertEquals(sphericalCoordinate.getLatitude(), convertedCoordinate.getLatitude(), 0);
+    }
+
+    @Test
+    public void testOnYAxisConvertToSpherical() {
+        CartesianCoordinate cartesianCoordinate = new CartesianCoordinate(0, 6371, 0);
+        SphericalCoordinate sphericalCoordinate = new SphericalCoordinate(90, 90);
+
+        SphericalCoordinate convertedCoordinate = cartesianCoordinate.convertToSpherical();
+
+        assertEquals(sphericalCoordinate.getLongitude(), convertedCoordinate.getLongitude(), 0);
+        assertEquals(sphericalCoordinate.getLatitude(), convertedCoordinate.getLatitude(), 0);
+    }
+
+    @Test
+    public void testOnZAxisConvertToSpherical() {
+        CartesianCoordinate cartesianCoordinate = new CartesianCoordinate(0, 0, 6371);
+        SphericalCoordinate sphericalCoordinate = new SphericalCoordinate(0, 0);
+
+        SphericalCoordinate convertedCoordinate = cartesianCoordinate.convertToSpherical();
+
+        assertEquals(sphericalCoordinate.getLongitude(), convertedCoordinate.getLongitude(), 0);
+        assertEquals(sphericalCoordinate.getLatitude(), convertedCoordinate.getLatitude(), 0);
     }
 }
